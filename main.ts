d4e5a8e65cdde47c134ca7e75df0d504bdcfcaf8
DeXin64 <129580034+DeXin64@users.noreply.github.com>
2024-05-06 09:16:24 +0800

    切换nezhav2积木块开发环境

--- main.ts
+++ main.ts
@@ -1,187 +1,377 @@
-/**
-* Functions to NeZha multifunctional expansion board by ELECFREAKS Co.,Ltd.
+enum MotorPostion {
+    //%block="M1"
+    M1 = 1,
+    //%block="M2"
+    M2 = 2,
+    //%block="M3"
+    M3 = 3,
+    //%block="M4"
+    M4 = 4
+}
+
+enum MovementDirection {
+    //%block="cw"
+    cw = 1,
+    //%block="ccw"
+    ccw = 2
+}
+
+enum SportsMode {
+    //%block="circle"
+    circle = 1,
+    //%block="degree"
+    degree = 2,
+    //%block="second"
+    second = 3
+}
+
+
+enum ServoMotionMode {
+    //%block="Shortest path"
+    ShortestPath = 1,
+    //%block="cw"
+    cw = 2,
+    //%block="ccw"
+    ccw = 3
+}
+
+
+enum MotorCombination {
+    //%block="A+B"
+    ab = 1,
+    //%block="A+C"
+    ac = 2,
+    //%block="A+D"
+    ad = 3,
+    //%block="B+C"
+    bc = 4,
+    //%block="B+D"
+    bd = 5,
+    //%block="C+D"
+    cd = 6
+}
+
+enum HorizontalDirection {
+    //%block="left"
+    left = 1,
+    //%block="right"
+    right = 2
+}
+
+enum VerticallDirection {
+    //%block="up"
+    up = 1,
+    //%block="dowm"
+    dowm = 2
+}
+
+enum Unit {
+    //%block="cm"
+    cm = 1,
+    //%block="irch"
+    irch = 2
+}
+//% weight=100 color=#008C8C block="NEHZAV2" blockId="NEHZAV2" icon="\uf48b"
+namespace NEHZAV2 {
+    let i2cAddr: number = 0x10;
+
+    //% group="Basic functions"
+    //% block="set nehza %MotorPostion %MovementDirection %speed  %SportsMode"
+    //% speed.min=0  speed.max=360
+    //% weight=400 // 减少宽度  
+    export function Motorspeed(motor: MotorPostion, direction: MovementDirection, speed: number, MotorFunction: SportsMode): void {
+        let buf = pins.createBuffer(7)
+        buf[0] = 0xFF;
+        buf[1] = motor;
+        buf[2] = direction;
+        buf[3] = 0x70;
+        buf[4] = (speed >> 8) & 0XFF;
+        buf[5] = MotorFunction;
+        buf[6] = (speed >> 0) & 0XFF;
+        pins.i2cWriteBuffer(i2cAddr, buf);
+        basic.pause(4)
+    }
+
+    //% group="Basic functions"
+    //% weight=140
+    //% block="nehza-motor goToAbsolutePosition %MotorPostion mode %ServoMotionMode anagle to %speed "
+    //% speed.min=0  speed.max=360
+    export function goToAbsolutePosition(motor: MotorPostion, modePostion: ServoMotionMode, speed: number): void {
+        let current_angle, target_angle, angle_diff;
+        target_angle = speed
+        current_angle = readServoAbsolutePostion(motor)//是要获取的
+        switch (modePostion) {
+            case 1:
+                let buf = pins.createBuffer(7)
+                buf[0] = 0xFF;
+                buf[1] = motor;//���λ��
+                buf[2] = modePostion;//
+                buf[3] = 0x66;
+                buf[4] = (speed >> 8) & 0XFF;
+                buf[5] = 0xF5;
+                buf[6] = (speed >> 0) & 0XFF;
+                pins.i2cWriteBuffer(i2cAddr, buf);
+                basic.pause(4)
+                break;
+            case 2:
+                //正转
+                angle_diff = (target_angle - current_angle + 360) % 360
+                NEHZAV2.Motorspeed(motor, MovementDirection.cw, angle_diff, SportsMode.degree)
+                break;
+            case 3:
+                //反转
+                angle_diff = (current_angle - target_angle + 360) % 360
+                NEHZAV2.Motorspeed(motor, MovementDirection.ccw, angle_diff, SportsMode.degree)
+                break;
+        }
+
+
+    }
+
+    //% group="Basic functions"
+    //% weight=140
+    //% block="Start the %MotorPostion %MovementDirection motor "
+    //% speed.min=0  speed.max=100
+    export function nezha2MotorStart(motor: MotorPostion, direction: MovementDirection): void {
+        let buf = pins.createBuffer(7)
+        buf[0] = 0xFF;
+        buf[1] = motor;
+        buf[2] = direction;
+        buf[3] = 0x5E;
+        buf[4] = 0x00;
+        buf[5] = 0xF5;
+        buf[6] = 0x00;
+        pins.i2cWriteBuffer(i2cAddr, buf);
+        basic.pause(4)
+    }
+
+    //% group="Basic functions"
+    //% weight=140
+    //% block="Stop the %MotorPostion motor "
+    //% speed.min=0  speed.max=100
+    export function nezha2MotorStop(motor: MotorPostion,): void {
+        let buf = pins.createBuffer(7)
+        buf[0] = 0xFF;
+        buf[1] = motor;
+        buf[2] = 0x00;
+        buf[3] = 0x5F;
+        buf[4] = 0x00;
+        buf[5] = 0xF5;
+        buf[6] = 0x00;
+        pins.i2cWriteBuffer(i2cAddr, buf);
+        basic.pause(4)
+    }
+
+    //% group="Basic functions"
+    //% weight=140
+    //% block="nehza-motor Crtol %MotorPostion speed %MovementDirection speed to %speed "
+    //% speed.min=0  speed.max=100
+    export function nezha2MotorSpeedCtrol(motor: MotorPostion, direction: MovementDirection, speed: number): void {
+        let buf = pins.createBuffer(7)
+        buf[0] = 0xFF;
+        buf[1] = motor;
+        buf[2] = direction;
+        buf[3] = 0x60;
+        buf[4] = speed;
+        buf[5] = 0xF5;
+        buf[6] = 0x00;
+        pins.i2cWriteBuffer(i2cAddr, buf);
+        basic.pause(4)
+    }
+
+    //% group="Basic functions"
+    //% weight=320
+    //%block="get %MotorPostion servo of postion"
+    export function readServoAbsolutePostion(motor: MotorPostion): number {
+        let ServoSpeedArr = pins.createBuffer(4);
+        let buf = pins.createBuffer(7);
+        buf[0] = 0xFF;
+        buf[1] = motor;
+        buf[2] = 0x00;
+        buf[3] = 0x46; // ????????  
+        buf[4] = 0x00;
+        buf[5] = 0xF5; // ????????  
+        buf[6] = 0x00;
+        pins.i2cWriteBuffer(i2cAddr, buf);
+        basic.pause(50); // ????  
+
+        // ??4?????  
+        ServoSpeedArr[0] = pins.i2cReadNumber(i2cAddr, NumberFormat.UInt8LE, false);
+        ServoSpeedArr[1] = pins.i2cReadNumber(i2cAddr, NumberFormat.UInt8LE, false);
+        ServoSpeedArr[2] = pins.i2cReadNumber(i2cAddr, NumberFormat.UInt8LE, false);
+        ServoSpeedArr[3] = pins.i2cReadNumber(i2cAddr, NumberFormat.UInt8LE, false);
+        basic.pause(10); // ???????????????  
+
+
+        let ServoSpeed = (ServoSpeedArr[3] << 24) | (ServoSpeedArr[2] << 16) | (ServoSpeedArr[1] << 8) | (ServoSpeedArr[0]);
+        if ((ServoSpeed << 31) != 1) {
+            ServoSpeed = ((ServoSpeed & 0x0fffff) / 10) % 360
+        }
+        else {
+            ServoSpeed = ((0x0fffff - (ServoSpeed & 0x0fffff)) / 10) % 360
+        }
+
+        return ServoSpeed;
+    }
+
+    //% group="Basic functions"
+    //% weight=320
+    //%block="get %MotorPostion servo of speed"
+    export function readServoAbsoluteSpeed(motor: MotorPostion): number {
+        let ServoAbsolutePostion: number
+        let ServoSpeed1Arr = pins.createBuffer(2);
+        let buf = pins.createBuffer(7)
+        buf[0] = 0xFF;
+        buf[1] = motor;
+        buf[2] = 0x00;
+        buf[3] = 0x47;
+        buf[4] = 0x00;
+        buf[5] = 0xF5;
+        buf[6] = 0x00;
+        pins.i2cWriteBuffer(i2cAddr, buf);
+        basic.pause(50);
+        ServoSpeed1Arr[0] = pins.i2cReadNumber(i2cAddr, NumberFormat.UInt8LE, false);
+        ServoSpeed1Arr[1] = pins.i2cReadNumber(i2cAddr, NumberFormat.UInt8LE, false);
+        let Servo1Speed = (ServoSpeed1Arr[1] << 8) | (ServoSpeed1Arr[0]);
+        Servo1Speed = Servo1Speed / 10
+        basic.pause(10);
+        return Servo1Speed;
+    }
+
+
+    //% group="Application functions"
+    //% weight=320
+    //%block="Set the running motor to %MotorCombination"
+    /*
+    组合积木块1：选择电机组合6种
+    */
+
+    export function RunningMotorToeSpeed(motor: MotorCombination): void {
+        let ServoAbsolutePostion: number
+
+    }
+
+    /*
+     组合积木块2：设置组合电机速度
+ */
+    //% group="Application functions"
+    //% weight=320
+    //%block="Set the running Combination Motor to %speed"
+    //% speed.min=0  speed.max=100
+
+    export function SetMotionSpeed(speed: number): void {
+        let ServoAbsolutePostion: number
+
+    }
+
+    /*
+    组合积木块3：电机功能，设置组合电机，设置方向，设置运动模式，设置运动参数
+左上角，左下角，右上角，右下角
 */
-//% color=#ff0000  icon="\uf06d" block="NeZha" blockId="NeZha"
-namespace neZha {
-    const neZha_address = 0x10
-	/**
-	* MotorList
-	*/
-    export enum MotorList {
-        //% block="M1"
-        M1,
-        //% block="M2"
-        M2,
-        //% block="M3"
-        M3,
-        //% block="M4"
-        M4
-    }
-	/**
-	* ServoList
-	*/
-    export enum ServoList {
-        //% block="S1" 
-        S1,
-        //% block="S2"
-        S2,
-        //% block="S3" 
-        S3,
-        //% block="S4"
-        S4
-    }
-    export enum ServoTypeList {
-        //% block="180°" 
-        _180,
-        //% block="270°"
-        _270,
-        //% block="360°" 
-        _360
-    }
-	/**
-     * TODO: Set the speed of M1, M2, M3, M4 motor. 
-     * @param motor M1, M2, M3, M4 motor , eg: MotorList.M1
-     * @param speed motor speed, eg: 100
-     */
-    //% weight=88
-    //% blockId=setMotorSpeed block="Set motor %motor speed to %speed\\%"
-    //% speed.min=-100 speed.max=100
-    export function setMotorSpeed(motor: MotorList, speed: number): void {
-        let iic_buffer = pins.createBuffer(4);
-        if (speed > 100) {
-            speed = 100
-        }
-        else if(speed < -100){
-            speed = -100
-        }
-        switch (motor) {
-            case MotorList.M1:
-                iic_buffer[0] = 0x01;
-                if (speed >= 0) {
-                    iic_buffer[1] = 0x01;
-                }
-                else {
-                    iic_buffer[1] = 0x02;
-                    speed = speed * -1
-                }
-                iic_buffer[2] = speed;
-                iic_buffer[3] = 0;
-                pins.i2cWriteBuffer(neZha_address, iic_buffer);
-                break;
-            case MotorList.M2:
-                iic_buffer[0] = 0x02;
-                if (speed >= 0) {
-                    iic_buffer[1] = 0x01;
-                }
-                else {
-                    iic_buffer[1] = 0x02;
-                    speed = speed * -1
-                }
-                iic_buffer[2] = speed;
-                iic_buffer[3] = 0;
-                pins.i2cWriteBuffer(neZha_address, iic_buffer);
-                break;
-            case MotorList.M3:
-                iic_buffer[0] = 0x03;
-                if (speed >= 0) {
-                    iic_buffer[1] = 0x01;
-                }
-                else {
-                    iic_buffer[1] = 0x02;
-                    speed = speed * -1
-                }
-                iic_buffer[2] = speed;
-                iic_buffer[3] = 0;
-                pins.i2cWriteBuffer(neZha_address, iic_buffer);
-                break;
-            case MotorList.M4:
-                iic_buffer[0] = 0x04;
-                if (speed >= 0) {
-                    iic_buffer[1] = 0x01;
-                }
-                else {
-                    iic_buffer[1] = 0x02;
-                    speed = speed * -1
-                }
-                iic_buffer[2] = speed;
-                iic_buffer[3] = 0;
-                pins.i2cWriteBuffer(neZha_address, iic_buffer);
-                break;
-            default:
-                break;
-        }
+    //% group="Application functions"
+    //% weight=320
+    //%block="Motor Move to %HorizontalDirection %speed %SportsMode"
+    //% speed.min=0  speed.max=360
+
+    export function CombinationMotorspeed(horizontalDirection: HorizontalDirection, speed: number, MotorFunction: SportsMode): void {
+        let ServoAbsolutePostion: number
+
     }
 
-	/*
-     * TODO: Stop one of the motors. 
-     * @param motor A motor in the MotorList , eg: MotorList.M1
-     */
-    //% weight=86
-    //% blockId=stopMotor block="Stop motor %motor"
-    export function stopMotor(motor: MotorList): void {
-        setMotorSpeed(motor, 0)
-    }
-	/*
-     * TODO: Stop all motors, including M1, M2, M3, M4.
-     */
-    //% weight=85
-    //% blockId=stopAllMotor  block="Stop all motor"
-    export function stopAllMotor(): void {
-        setMotorSpeed(MotorList.M1, 0)
-        setMotorSpeed(MotorList.M2, 0)
-        setMotorSpeed(MotorList.M3, 0)
-        setMotorSpeed(MotorList.M4, 0)
+    /*
+    组合积木块4：舵机功能，设置组合电机，设置水平方向，设置运动参数
+左上角，左下角，右上角，右下角
+*/
+    //% group="Application functions"
+    //% weight=320
+    //%block="Servo Move to %HorizontalDirection %speed"
+    //% speed.min=0  speed.max=360
+
+    export function CombinationServoSpeed(horizontalDirection: HorizontalDirection, speed: number): void {
+        let ServoAbsolutePostion: number
+
     }
 
-	/*
-     * TODO: Setting the angle of a servo motor. 
-     * @param servo A servo in the ServoList , eg: ServoList.S1
-     * @param angel Angle of servo motor , eg: 90
-     */
-    //% weight=84
-    //% blockId=setServoAngel block="Set %servoType servo %servo angel to %angle°"
-    export function setServoAngel(servoType:ServoTypeList,servo: ServoList, angel: number): void {
-        let iic_buffer = pins.createBuffer(4);
-        switch (servo) {
-            case ServoList.S1:
-                iic_buffer[0] = 0x10;
-                break;
-            case ServoList.S2:
-                iic_buffer[0] = 0x11;
-                break;
-            case ServoList.S3:
-                iic_buffer[0] = 0x12;
-                break;
-            case ServoList.S4:
-                iic_buffer[0] = 0x13;
-                break;
-        }
-        switch(servoType){
-            case ServoTypeList._180:
-                angel = Math.map(angel, 0, 180, 0, 180)
-                break
-            case ServoTypeList._270:
-                angel = Math.map(angel, 0, 270, 0, 180)
-                break
-            case ServoTypeList._360:
-                angel = Math.map(angel, 0, 360, 0, 180)
-                break
-        }
-        iic_buffer[1] = angel;
-        iic_buffer[2] = 0;
-        iic_buffer[3] = 0;
-        pins.i2cWriteBuffer(neZha_address, iic_buffer);
+    /*
+    组合积木块5：停止组合电机转动
+*/
+    //% group="Application functions"
+    //% weight=320
+    //%block="Stop Combination Motor"
+    export function StopCombinationMotor(): void {
+        let ServoAbsolutePostion: number
+
+    }
+
+    /*
+    组合积木块6：组合电机垂直方向运动（一直运动）
+*/
+    //% group="Application functions"
+    //% weight=320
+    //%block="Combination Motor Move to %VerticallDirection"
+
+    export function CombinationMotorVerticallDirectionMove(verticallDirection: VerticallDirection): void {
+        let ServoAbsolutePostion: number
+
     }
     /*
-     * TODO: Setting the speed of a servo motor. 
-     * @param servo A servo in the ServoList , eg: ServoList.S1
-     * @param angel Angle of servo motor , eg: 100
-     */
-    //% weight=83
-    //% blockId=setServoSpeed block="Set continuous rotation servo %servo speed to %speed\\%"
-    //% speed.min=-100 speed.max=100
-    export function setServoSpeed(servo: ServoList, speed: number): void {
-        speed = Math.map(speed, -100, 100, 0, 180)
-        setServoAngel(ServoTypeList._180, servo, speed)
+    组合积木块7：组合舵机垂直方向运动（一直运动）
+*/
+    //% group="Application functions"
+    //% weight=320
+    //%block="Combination Motor Move to %VerticallDirection %speed %SportsMode "
+    //% speed.min=0  speed.max=360
+    export function CombinationServoVerticallDirectionMove(verticallDirection: VerticallDirection, speed: number, MotorFunction: SportsMode): void {
+        let ServoAbsolutePostion: number
+
+    }
+
+    /*
+    组合积木块8：将电机旋转一圈设置为（N）（厘米）
+*/
+    //% group="Application functions"
+    //% weight=320
+    //%block="Set the motor to rotate one revolution to %far %Unit"
+    export function SetMotorOneRotateRevolution(far: number, unit: Unit): void {
+        let ServoAbsolutePostion: number
+
+    }
+
+    //% group="test functions"
+    //% weight=140
+    //% block="tset %speed "
+    //% speed.min=0  speed.max=360
+    export function test(speed: number): void {
+        let buf = pins.createBuffer(6)
+        buf[0] = 0xFF;
+        buf[1] = 0x01;//���λ��
+        buf[2] = 0x00;//(speed >> 8) & 0xff;/
+        buf[3] = 0X99;//TEST MODE
+        buf[4] = (speed >> 8) & 0xff;//���λ��        
+        buf[5] = (speed >> 0) & 0xff;//���λ��
+        pins.i2cWriteBuffer(i2cAddr, buf);
+    }
+
+    //% group="export functions"
+    //% weight=320
+    //%block="get %MotorPostion servo of postion"
+    export function readVersion(): string {
+        let versionH, versionZ, versionL;
+        let buf = pins.createBuffer(7);
+        buf[0] = 0xFF;
+        buf[1] = 0x00;
+        buf[2] = 0x00;
+        buf[3] = 0x88; // ????????  
+        buf[4] = 0x00;
+        buf[5] = 0x00; // ????????  
+        buf[6] = 0x00;
+        pins.i2cWriteBuffer(i2cAddr, buf);
+        basic.pause(50); // ????  
+
+        // ??4?????  
+        versionH = pins.i2cReadNumber(i2cAddr, NumberFormat.UInt8LE, false);
+        basic.pause(10); // ???????????????  
+
+        return ("V" + convertToText(1) + "." + convertToText(0) + "." + convertToText(versionH))
     }
 }
